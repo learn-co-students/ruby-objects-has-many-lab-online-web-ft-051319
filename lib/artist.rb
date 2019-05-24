02d670a853cddaf38d710bc8f098b72b43278d6d
Deborah Barcenas <deborah.barcenas@gmail.com>
2019-05-24 20:37:37 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,32 +2,34 @@
 class Artist
   attr_accessor :name
 
-   @@song_count = []
+  @@all = []
 
    def initialize(name)
     @name = name
-    @songs = []
+    @@all << self
   end
 
    def add_song(song)
-    @songs << song
     song.artist = self
-    @@song_count << song
   end
 
    def add_song_by_name(name)
     song = Song.new(name)
-    @songs << song
     song.artist = self
-    @@song_count << song
   end
 
    def songs
-    @songs
+    Song.all.map do |song|
+      song.artist == self
+    end
   end
 
    def self.song_count
-    @@song_count.size
+     counter = 0
+     self.all.each do |artist|
+       counter += artist.songs.counter
+     end
+     counter
   end
 
  end
