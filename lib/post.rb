3055e3b56d3d507f43f2549c1b300441d96f771d
Ibrahim  Akar <ibsakar@gmail.com>
2019-06-02 16:11:29 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,25 @@
+class Post
+    attr_accessor :title, :author
+    
+    @@all=[]
+  
+  def initialize(title)
+    @title = title
+   # @@all << title 
+    @@all << self
+  end
+
+  def self.all
+      @@all
+  end
+  
+  def author_name
+      
+      if author == nil
+        return nil 
+      else 
+        self.author = author.name
+  end
+end
+
+end
